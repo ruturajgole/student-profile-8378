1634ce3c2c701d1f6339d69ec9239e63c8830487
Ruturaj Gole <ruturajpokemon1@gmail.com>
2022-03-02 20:57:34 -0700

    Make list items expandable

--- src/lib/list-item/index.tsx
+++ src/lib/list-item/index.tsx
@@ -1,7 +1,26 @@
 import { Student } from "app/services/models";
+import { Component } from "react";
 
-export const StudentListItem: React.FunctionComponent<Props> = ({student}) =>
-    <div style={styles.container}>
+export class StudentListItem extends Component<Props, State> {
+    constructor(props: Props){
+        super(props);
+
+        this.state = {
+            isExpanded: false
+        }
+
+        this.onToggle = this.onToggle.bind(this);
+    }
+
+    public onToggle(){
+        this.setState(({ isExpanded }) => ({ isExpanded: !isExpanded }));
+    }
+
+    render(){
+    const { student } = this.props;
+    const { isExpanded } = this.state;
+
+    return (<div style={styles.container}>
         <img style={styles.image} src={student.pic}/>
         <div style={styles.textContainer}>
             <p style={styles.name}>{student.firstName.toUpperCase()} {student.lastName.toUpperCase()}</p>
@@ -9,11 +28,31 @@ export const StudentListItem: React.FunctionComponent<Props> = ({student}) =>
             <p style={styles.details}>Company: {student.company}</p>
             <p style={styles.details}>Skill: {student.skill}</p>
             <p style={styles.details}>Average: {student.average}%</p>
+            {isExpanded && 
+                <ul style={styles.list}>
+                    {
+                    student.grades.map((grade, index) =>
+                        <li
+                            style={styles.listItem} 
+                            key={index}>
+                            <span>Test {index + 1}:</span>
+                            <span>{grade}%</span>
+                        </li>)
+                    }
+                </ul>
+            }
         </div>
-    </div>;
+        <button style={styles.button} onClick={this.onToggle}>{isExpanded ? "-" : "+"}</button>
+    </div>);
+    }
+}
 
 interface Props {
     readonly student: Student;
+}
+
+interface State {
+    readonly isExpanded: boolean;
 }
 
 /** Styles */
@@ -23,7 +62,6 @@ const styles: { [key: string]: React.CSSProperties } = {
         backgroundColor: "white",
         display: "flex",
         flexDirection: "row",
-        alignItems: "center",
     },
     textContainer: {
         lineHeight: "5px",
@@ -45,5 +83,26 @@ const styles: { [key: string]: React.CSSProperties } = {
     details: {
         margin: "20px",
         fontFamily:"Raleway Light",
+    },
+    list: {
+        listStyle: "none",
+        lineHeight: "normal",
+        fontFamily: "Raleway Light",
+        margin: "20px",
+        padding: "0",
+    },
+    listItem: {
+        display: "flex",
+        width: "50%",
+        justifyContent: "space-between",
+    },
+    button: {
+        backgroundColor: "transparent",
+        borderStyle: "none",
+        fontSize: "xxx-large",
+        color: "darkgray",
+        alignSelf: "start",
+        marginLeft: "auto",
+        cursor: "pointer",
     }
   };
