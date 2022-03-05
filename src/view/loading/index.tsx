5b2c05c0f8cfa209be52f431ced0159afdf57cc4
Ruturaj Gole <ruturajpokemon1@gmail.com>
2022-03-04 23:01:20 -0700

    Add loading text

--- src/view/loading/index.tsx
+++ src/view/loading/index.tsx
@@ -1,10 +1,21 @@
-import React from "react";
-
 export const LoadingView = () =>
-  <div>
-    Loading
+  <div style={styles.container}>
+    <span style={styles.text}>Getting Values</span>
   </div>;
 
 
 /** Styles */
 
+const styles: { [key: string]: React.CSSProperties } = {
+  container: {
+    height: "100%",
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+  },
+  text: {
+    fontFamily: "Raleway",
+    fontSize: "xxx-large"
+  }
+}
